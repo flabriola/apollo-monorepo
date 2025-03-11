d96f7abb956cc4baf3a602707d4ffedae0352969
joaolabriola <jsb2000@hw.ac.uk>
2025-03-11 09:43:11 +0400

    Revert "Connected backend to secure AWS RDS MySQL database"
    This reverts commit 1614427987bb48fc9735b9704fb52f8eeb08ed20.

--- server.js
+++ server.js
@@ -1,35 +1,13 @@
 require('dotenv').config();
-const mysql = require('mysql2');
 const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// ✅ MySQL Database Connection
-const db = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
-});
-
-// Handle database connection errors
-db.connect(err => {
-  if (err) {
-    console.error('❌ Database connection failed:', err.message);
-    process.exit(1); // Exit process if DB connection fails
-  } else {
-    console.log('✅ Connected to MySQL Database!');
-  }
-});
-
-// Basic API Route
 app.get('/', (req, res) => {
-  res.send('Apollo Guide Backend is Live with Nodemon!');
+    res.send('Apollo Guide Backend is Live with Nodemon!');
 });
 
-// Start the Express Server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
