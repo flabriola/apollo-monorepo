4b2c8a8c5a636ed3bfd951669d4b155ca94de4b5
flabriola <labriolabianchi@gmail.com>
2025-05-10 14:14:16 +0400

    Amplify new build instructions 22

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  },
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    outDir: 'dist'
   }
 })
