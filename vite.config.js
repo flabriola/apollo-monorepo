655b27cc80c699b7e76814429ab0ea97690da7fa
flabriola <labriolabianchi@gmail.com>
2025-05-10 14:04:10 +0400

    Amplify new build instructions rollup

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+    target: 'esnext',
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom']
