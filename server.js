33b75824314f9fe6a3c6886e69c6758036c63a62
flabriola <labriolabianchi@gmail.com>
2025-05-29 13:09:54 +0400

    JSON Limit increased

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Set up AWS configuration
 AWS.config.update({
