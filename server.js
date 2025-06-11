8f2024f1ef4076ac4ddca35e59bf8fceccad8699
flabriola <labriolabianchi@gmail.com>
2025-06-11 10:17:18 +0400

    Full restaurant data route

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ const restaurantPool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_RESTAURANT_NAME,
+  database: process.env.DB_MAIN_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
@@ -231,6 +231,135 @@ app.get('/api/restaurant-data/:id', (req, res) => {
     res.json(results);
   });
 });
+
+// app.get('/api/restaurant-data/:id', (req, res) => {
+//   const restaurantId = req.params.id;
+//   const query = `
+//     SELECT JSON_OBJECT(
+//     'name',           r.name,
+//     'address',        r.address,
+//     'phone',          r.phone,
+//     'logo_url',       r.logo_url,
+//     'website',        r.website_url,
+//     'menu_list',      (
+//         SELECT JSON_ARRAYAGG(
+//         JSON_OBJECT(
+//             'id',          m.id,
+//             'name',        m.name,
+//             'description', m.description,
+//             'active',      m.active
+//         )
+//         )
+//         FROM menu AS m
+//         WHERE m.restaurant_id = r.id
+//     ),
+//     'menus',          (
+//         SELECT JSON_ARRAYAGG(
+//         JSON_OBJECT(
+//             'name',        m.name,
+//             'description', m.description,
+//             'active',      m.active,
+//             'pdf_url',     m.pdf_url,
+//             'dishes',      (
+//             SELECT JSON_OBJECTAGG(
+//                 d.id,
+//                 JSON_OBJECT(
+//                 'name',        d.name,
+//                 'description', d.description,
+//                 'category',    d.category,
+//                 'active',      d.active,
+                
+//                 -- de-duplicated allergens
+//                 'allergens', (
+//                     SELECT JSON_ARRAYAGG(
+//                     JSON_OBJECT(
+//                         'id',          aa.allergen_id,
+//                         'ingredients', aa.ingredients
+//                     )
+//                     )
+//                     FROM (
+//                     SELECT
+//                         ia.allergen_id,
+//                         JSON_ARRAYAGG(
+//                         JSON_OBJECT('id', i.id, 'name', i.name)
+//                         ) AS ingredients
+//                     FROM dish_ingredient AS di
+//                     JOIN ingredient            AS i  ON i.id = di.ingredient_id
+//                     JOIN ingredient_allergen   AS ia ON ia.ingredient_id = i.id
+//                     WHERE di.dish_id = d.id
+//                     GROUP BY ia.allergen_id
+//                     ) AS aa
+//                 ),
+                
+//                 -- de-duplicated diets
+//                 'diets', (
+//                     SELECT JSON_ARRAYAGG(
+//                     JSON_OBJECT(
+//                         'id',          dd.diet_id,
+//                         'ingredients', dd.ingredients
+//                     )
+//                     )
+//                     FROM (
+//                     SELECT
+//                         idr.diet_id,
+//                         JSON_ARRAYAGG(
+//                         JSON_OBJECT('id', i.id, 'name', i.name)
+//                         ) AS ingredients
+//                     FROM dish_ingredient AS di
+//                     JOIN ingredient         AS i   ON i.id = di.ingredient_id
+//                     JOIN ingredient_diet    AS idr ON idr.ingredient_id = i.id
+//                     WHERE di.dish_id = d.id
+//                     GROUP BY idr.diet_id
+//                     ) AS dd
+//                 ),
+                
+//                 -- de-duplicated cross-contaminations
+//                 'cross_contaminations', (
+//                     SELECT JSON_ARRAYAGG(
+//                     JSON_OBJECT(
+//                         'allergen',        cc.allergen_id,
+//                         'reason',          cc.reason,
+//                         'ingredient',      cc.ingredient_id,
+//                         'ingredient_name', cc.ingredient_name,
+//                         'description',     cc.description
+//                     )
+//                     )
+//                     FROM (
+//                     SELECT DISTINCT
+//                         dca.allergen_id,
+//                         dca.reason,
+//                         dca.ingredient_id,
+//                         i3.name           AS ingredient_name,
+//                         dca.description
+//                     FROM dish_cc_allergen AS dca
+//                     LEFT JOIN ingredient AS i3 ON i3.id = dca.ingredient_id
+//                     WHERE dca.dish_id = d.id
+//                     ) AS cc
+//                 )
+//                 )
+//             )
+//             FROM dish AS d
+//             WHERE d.menu_id = m.id
+//                 AND d.active = TRUE
+//             )
+//         )
+//         )
+//         FROM menu AS m
+//         WHERE m.restaurant_id = r.id
+//         AND m.active = TRUE
+//     )
+//     ) AS restaurant_data
+//     FROM restaurant AS r
+//     WHERE r.id = ?;
+//   `;
+//   ingredientsPool.query(query, (err, results) => {
+//     if (err) {
+//       console.error('Failed to fetch ingredients with allergens and diets:', err.message);
+//       return res.status(500).json({ error: 'Database error' });
+//     }
+//     res.json(results);
+//   });
+// });
 
 
 app.listen(PORT, () => {
