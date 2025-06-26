58416b7a5e03769b1199f6b02185c77d294051b9
flabriola <labriolabianchi@gmail.com>
2025-06-26 17:35:37 +0400

    Genesis

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://192.168.1.212:5173',    // Home
   'http://172.16.100.195:5173',   // Studio
+  'http://192.168.40.89:5173',    // Genesis
   // Local Screening app
   'http://127.0.0.1:5173',
   // Screening app
